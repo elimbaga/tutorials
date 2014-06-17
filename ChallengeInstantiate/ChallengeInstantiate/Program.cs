28d4776beeaf3c66676d7fd453908f26e7d5948a
elimbaga <elimbaga@lancera.com>
2014-06-17 15:44:16 +0800

    Initial commit for C# exercise
    Added the files for the C# tutorials.

---
+++ ChallengeInstantiate/ChallengeInstantiate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeInstantiate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Car montero = new Car();
+            //montero.store = "Somewhere";
+
+            //montero.Drive();
+            //montero.Stop();
+            string retVal;
+            retVal = Add(1,2);
+
+             
+
+
+        }
+
+
+        static string Add(int numbah1, int numbah2){
+
+            int result = numbah1 + numbah2;
+            string answer = "Success";
+            Console.WriteLine(result);
+
+            return answer;
+        }
+    }
+}
